f929967a34addf6dfde516146e3c7212083b1149
Dmitry Khvorov <dmitry.khvorov@gmail.com>
2018-09-30 15:21:32 +0800

    milestone: RecordedBasicPublisher works

--- src/RecordingPos.cpp
+++ src/RecordingPos.cpp
@@ -17,22 +17,53 @@
 
 #include "RecordingPos.h"
 
+namespace {
+
+constexpr std::int32_t SIZE_OF_INT = sizeof(std::int32_t);
+constexpr std::int32_t SIZE_OF_LONG = sizeof(std::int64_t);
+
+constexpr std::int32_t TYPE_ID_OFFSET = sizeof(std::int32_t);
+constexpr std::int32_t RECORDING_POSITION_TYPE_ID = 100;
+constexpr std::int32_t RECORDING_ID_OFFSET = 0;
+constexpr std::int32_t SESSION_ID_OFFSET = RECORDING_ID_OFFSET + SIZE_OF_LONG;
+constexpr std::int32_t SOURCE_IDENTITY_LENGTH_OFFSET = SESSION_ID_OFFSET + SIZE_OF_INT;
+constexpr std::int32_t SOURCE_IDENTITY_OFFSET = SOURCE_IDENTITY_LENGTH_OFFSET + SIZE_OF_INT;
+}  // namespace
+
 namespace aeron {
 namespace archive {
 
-std::int32_t RecordingPos::findCounterIdBySession(aeron::concurrent::CountersReader& countersReader,
-                                                  std::int32_t sessionId) {
+std::int32_t RecordingPos::findCounterIdBySession(concurrent::CountersReader& countersReader, std::int32_t sessionId) {
+    auto buffer = countersReader.metaDataBuffer();
+
+    for (int i = 0, size = countersReader.maxCounterId(); i < size; ++i) {
+        if (countersReader.getCounterState(i) == concurrent::CountersReader::RECORD_ALLOCATED) {
+            std::int32_t recordOffset = concurrent::CountersReader::metadataOffset(i);
+
+            if (buffer.getInt32(recordOffset + TYPE_ID_OFFSET) == RECORDING_POSITION_TYPE_ID && buffer.getInt32(recordOffset + concurrent::CountersReader::KEY_OFFSET + SESSION_ID_OFFSET) == sessionId) {
+                return i;
+            }
+        }
+    }
+
     return -1;
 }
 
-std::int64_t RecordingPos::getRecordingId(aeron::concurrent::CountersReader& countersReader, std::int32_t counterId) {
+std::int64_t RecordingPos::getRecordingId(concurrent::CountersReader& countersReader, std::int32_t counterId) {
+    auto buffer = countersReader.metaDataBuffer();
+
+    if (countersReader.getCounterState(counterId) == concurrent::CountersReader::RECORD_ALLOCATED) {
+        std::int32_t recordOffset = concurrent::CountersReader::metadataOffset(counterId);
+
+        if (buffer.getInt32(recordOffset + TYPE_ID_OFFSET) == RECORDING_POSITION_TYPE_ID) {
+            return buffer.getInt64(recordOffset + concurrent::CountersReader::KEY_OFFSET + RECORDING_ID_OFFSET);
+        }
+    }
+
     return -1;
 }
 
-bool RecordingPos::isActive(aeron::concurrent::CountersReader& countersReader, std::int32_t counterId,
-                            std::int64_t recordingId) {
-    return false;
-}
+bool RecordingPos::isActive(concurrent::CountersReader& countersReader, std::int32_t counterId, std::int64_t recordingId) { return RecordingPos::getRecordingId(countersReader, counterId) == recordingId; }
 
 }  // namespace archive
 }  // namespace aeron
