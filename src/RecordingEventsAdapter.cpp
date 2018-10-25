3909e5b0c053244b02df33491a073ebaa79ebba3
Dmitry Khvorov <dmitry@fairtide.co>
2018-10-25 16:42:29 +0800

    added RecordingEventsAdapter

---
+++ src/RecordingEventsAdapter.cpp
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2018 Fairtide Pte. Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "io_aeron_archive_codecs/RecordingProgress.h"
+#include "io_aeron_archive_codecs/RecordingStarted.h"
+#include "io_aeron_archive_codecs/RecordingStopped.h"
+
+#include "ArchiveException.h"
+#include "RecordingEventsAdapter.h"
+
+namespace codecs = io::aeron::archive::codecs;
+
+namespace aeron {
+namespace archive {
+
+RecordingEventsAdapter::RecordingEventsAdapter(const std::shared_ptr<aeron::Subscription>& subscription,
+                                               std::int32_t fragmentLimit, OnStart&& onStart, OnProgress&& onProgress,
+                                               OnStop&& onStop)
+    : subscription_(subscription),
+      fragmentLimit_(fragmentLimit),
+      onStart_(onStart),
+      onProgress_(onProgress),
+      onStop_(onStop) {
+    fragmentHandler_ = [this](aeron::AtomicBuffer& buffer, aeron::util::index_t offset, aeron::util::index_t length,
+                              const aeron::Header& header) { fragmentHandler(buffer, offset, length, header); };
+}
+
+std::int32_t RecordingEventsAdapter::poll() { return subscription_->poll(fragmentHandler_, fragmentLimit_); }
+
+void RecordingEventsAdapter::fragmentHandler(aeron::AtomicBuffer& buffer, aeron::util::index_t offset,
+                                             aeron::util::index_t length, const aeron::Header& header) {
+    codecs::MessageHeader hdr;
+    hdr.wrap((char*)buffer.buffer(), offset, 0, buffer.capacity());
+
+    const std::uint16_t templateId = hdr.templateId();
+
+    if (templateId == codecs::RecordingStarted::sbeTemplateId()) {
+        codecs::RecordingStarted msg;
+        msg.wrapForDecode((char*)buffer.buffer(), offset + hdr.encodedLength(), hdr.blockLength(), hdr.version(),
+                          buffer.capacity());
+
+        onStart_(msg.recordingId(), msg.startPosition(), msg.sessionId(), msg.streamId(), msg.getChannelAsString(),
+                 msg.getSourceIdentityAsString());
+    } else if (templateId == codecs::RecordingProgress::sbeTemplateId()) {
+        codecs::RecordingProgress msg;
+        msg.wrapForDecode((char*)buffer.buffer(), offset + hdr.encodedLength(), hdr.blockLength(), hdr.version(),
+                          buffer.capacity());
+
+        onProgress_(msg.recordingId(), msg.startPosition(), msg.position());
+    } else if (templateId == codecs::RecordingStopped::sbeTemplateId()) {
+        codecs::RecordingStopped msg;
+        msg.wrapForDecode((char*)buffer.buffer(), offset + hdr.encodedLength(), hdr.blockLength(), hdr.version(),
+                          buffer.capacity());
+
+        onStop_(msg.recordingId(), msg.startPosition(), msg.stopPosition());
+    } else {
+        throw ArchiveException("unknown template id: " + std::to_string(templateId), SOURCEINFO);
+    }
+}
+
+}  // namespace archive
+}  // namespace aeron
---
+++ src/RecordingEventsAdapter.h
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2018 Fairtide Pte. Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <Aeron.h>
+
+namespace aeron {
+namespace archive {
+
+class RecordingEventsAdapter {
+public:
+    using OnStart =
+        std::function<void(std::int64_t recordingId, std::int64_t startPosition, std::int32_t sessionId,
+                           std::int32_t streamId, const std::string& channel, const std::string& sourceIdentity)>;
+    using OnProgress = std::function<void(std::int64_t recordingId, std::int64_t startPosition, std::int64_t position)>;
+    using OnStop = std::function<void(std::int64_t recordingId, std::int64_t startPosition, std::int64_t position)>;
+
+    RecordingEventsAdapter(const std::shared_ptr<aeron::Subscription>& subscription, std::int32_t fragmentLimit,
+                           OnStart&& onStart, OnProgress&& onProgress, OnStop&& onStop);
+
+    std::int32_t poll();
+
+private:
+    void fragmentHandler(aeron::AtomicBuffer& buffer, aeron::util::index_t offset, aeron::util::index_t length,
+                         const aeron::Header& header);
+
+private:
+    std::shared_ptr<aeron::Subscription> subscription_;
+    const std::int32_t fragmentLimit_;
+
+    OnStart onStart_;
+    OnProgress onProgress_;
+    OnStop onStop_;
+
+    aeron::fragment_handler_t fragmentHandler_;
+};
+
+}  // namespace archive
+}  // namespace aeron
